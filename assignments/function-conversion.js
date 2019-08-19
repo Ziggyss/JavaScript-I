3b5e3fd3366ce3cd966c9f957fb5a27f2a5c5044
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-08-19 14:22:56 +0100

    finish function conversion examples

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -20,11 +20,16 @@ anotherFunction("Example");
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => param1 + param2;
+add (1,2);
+
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
+subtract(1,2);
 
 
 // Stretch
